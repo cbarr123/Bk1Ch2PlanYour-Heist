6d305d1f833128a20e9e847c37a7d6d1ed7bba02
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-04 11:06:58 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanYourHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan your heist!");
+
+            List<Dictionary<string, string>> team = 
+                new List<Dictionary<string, string>>();
+            
+            Console.WriteLine("Name>");
+            string name = Console.ReadLine();
+
+            while (name != "") {
+
+                Console.WriteLine("Skill Level>");
+                string skillLevel = Console.ReadLine();
+
+                Console.WriteLine("Courage Factor>");
+                string courageFactor = Console.ReadLine();
+
+                Dictionary<string, string> member = new Dictionary<string, string> () {
+                    {"Name", name},
+                    {"SkillLevel", skillLevel},
+                    {"CourageFactor", courageFactor}
+                };
+                team.Add(member);
+
+                Console.WriteLine("Name> ");
+                name = Console.ReadLine();
+
+
+        }
+            Console.WriteLine(
+                $"Team Size: {team.Count}"
+                
+            );
+        }
+    }
+}
